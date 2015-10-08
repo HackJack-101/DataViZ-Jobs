02666c2313b012587e7591ef862354c4960f2525
Jack3113 <jack03113@gmail.com>
2015-10-08 19:51:40 +0200

    Fix undefined error

--- geo.js
+++ geo.js
@@ -19,8 +19,7 @@ window.addEventListener('load', function () {
 	{
 		var title = jobs[i].getElementsByClassName('title')[0].innerHTML;
 		var address = jobs[i].getElementsByClassName('location')[0].innerHTML;
-		var description = jobs[i].getElementsByClassName('description')[0].innerHTML;
-		addMarker(address, title, description)
+		addMarker(address, title, "");
 	}
 });
 
